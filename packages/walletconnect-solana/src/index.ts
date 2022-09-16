4941adf464aea6fcc060f7778b459627e188bdc3
Jordan Sexton <jordan@jordansexton.com>
2022-09-16 12:16:55 -0500

    organize and lint imports / exports

--- packages/walletconnect-solana/src/index.ts
+++ packages/walletconnect-solana/src/index.ts
@@ -1,3 +1,3 @@
-export * from './adapter';
-export * from './errors';
+export * from './adapter.js';
+export * from './errors.js';
 export { default as WalletConnectClient } from '@walletconnect/sign-client';
